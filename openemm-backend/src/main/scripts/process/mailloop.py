5ac3480de7652986427015b725d5d59f973e2a2f
Anton Melser <anton.melser@delinin.com>
2013-11-17 15:40:52 +0100

    make improvements in auto mailloop processor

--- openemm-backend/src/main/scripts/process/mailloop.py
+++ openemm-backend/src/main/scripts/process/mailloop.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# vim: set fileencoding=utf-8 :
 import agn
 import re
 import os
@@ -10,20 +11,20 @@ agn.lock()
 agn.log(agn.LV_INFO, 'main', 'Starting up')
 db = agn.DBaseID()
 if db is None:
-        agn.die(s='Failed to setup database interface')
+    agn.die(s='Failed to setup database interface')
 db.log = lambda a: agn.log(agn.LV_DEBUG, 'db', a)
 
 curs = db.cursor()
 
 if curs is None:
-        agn.die(s='Failed to get database cursor')
+    agn.die(s='Failed to get database cursor')
 
 emails = set()
 
 for record in curs.query("select param from mailing_mt_tbl"):
-        m = re.search('from=".*?<(.*?)>', record[0])
-        if not m.group(1) is None:
-                emails.add(m.group(1))
+    m = re.search('from=".*?<(.*?)>', record[0])
+    if not m.group(1) is None:
+        emails.add(m.group(1))
 
 mailloop = "select concat('ext_', b.rid, '@', a.mailloop_domain) from company_tbl a inner join "
 mailloop += "mailloop_tbl b on a.company_id = b.company_id where forward_enable = 0 and "
@@ -32,24 +33,29 @@ mailloop += "ar_enable = 0 limit 1;"
 mailloopAddress = ""
 
 for record in curs.query(mailloop):
-        mailloopAddress = record[0]
+    mailloopAddress = record[0]
 
-localFilename = agn.base + os.sep + 'conf' + \
-    os.sep + 'bav' + os.sep + 'bav.conf-local'
-
-with open(localFilename, "r") as myfile:
+localFilename = agn.base + os.sep + 'conf' + os.sep + 'bav' + os.sep + 'bav.conf-local'
+try:
+    with open(localFilename, "r") as myfile:
         data = myfile.readlines()
+except Exception:
+    pass
 
 newmails = emails.copy()
 
 for email in emails:
-        for alias in data:
-                if alias.startswith(email):
-                        newmails.remove(email)
-                        break
-with open(localFilename, "w") as newfile:
+    for alias in data:
+        if alias.startswith(email):
+            newmails.remove(email)
+            break
+
+try:
+    with open(localFilename, "w") as newfile:
         for dataline in data:
-                if dataline.strip() != "":
-                        newfile.write(dataline.strip() + "\n")
+            if dataline.strip() != "":
+                newfile.write(dataline.strip() + "\n")
         for newmail in newmails:
-                newfile.write(newmail + "\talias:" + mailloopAddress + "\n")
+            newfile.write(newmail + "\talias:" + mailloopAddress + "\n")
+except:
+    agn.die(s='Unable to write bav.conf-local file! Async bounces will NOT be correctly processed!"
